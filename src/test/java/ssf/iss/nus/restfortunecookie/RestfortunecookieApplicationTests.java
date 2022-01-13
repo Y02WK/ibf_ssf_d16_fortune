91e6615598dce03c7d5877d93808fd3b33fb753b
Y02WK <59220086+Y02WK@users.noreply.github.com>
2022-01-13 13:46:11 +0800

    Initial commit

---
+++ src/test/java/ssf/iss/nus/restfortunecookie/RestfortunecookieApplicationTests.java
@@ -0,0 +1,13 @@
+package ssf.iss.nus.restfortunecookie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestfortunecookieApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
